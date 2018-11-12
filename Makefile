65ece88f07a4c2e8266b0d7ce56fbd141f17b1a4
myeonghyeon29 <audgus8435@naver.com>
2018-11-13 03:57:28 +0900

    make myas.h

--- Makefile
+++ Makefile
@@ -1,2 +1,14 @@
-# Write this Makefile
+myas: main.o trans.o check.o
+	gcc -o myas main.o trans.o check.o -lm
 
+main.o: main.c
+	gcc -O -Wall -c main.c
+
+trans.o: trans.c
+	gcc -O -Wall -c trans.c
+
+check.o: check.c
+	gcc -O -Wall -c check.c
+
+clean:
+	rm -f main.o trans.o check.o myas
--- trans.c
+++ trans.c
@@ -1,21 +1,35 @@
 #include "myas.h"
 
+#define EAX 21
+#define EBX 22
+#define ECX 23
+#define EDX 24
+#define ESI 25
+#define ESP 26
+#define EDI 27
+#define EBP 28
+
+int find_register(char* args){
+	int argstype;
+	if(args[2]=='a'){
+
+	}
+}
 int instr_trans(char *op, char *args, char* mcode)
 {
-	// check syntax292929 
+	// check syntax
+	char* tok;
+	char f_args[128];
+	char* b_args;
 	if(!is_valid(op, args)){
 		printf("Error: %s %s is not valid\n", op, args);
 		return 0;
 	}
+	tok=strtok(f_args,",");
+	b_args=strtok(NULL,"\0");
 
-
-	strcpy(mcode, "AB CD EF");
-
-	/********************************/
-    /*  							*/
-	/*    generate machine code     */
-    /*  							*/
-	/********************************/
-	
-	return 1;	
+	if(f_args[0]=='%'&&b_args[0]=='%')
+		strcpy(mcode,"89");
+	return 1;
 }
+
